3f139c17d1e15ce291215d58c1c8873316286693
Zhao Wei <zhaowei.pku@gmail.com>
2015-08-23 16:01:21 +0800

    gCampus small - 写文件头

---
+++ 2016 Round A C-gCampus.cpp
@@ -0,0 +1,21 @@
+#include <vector>
+#include <queue>
+#include <stack>
+#include <map>
+#include <set>
+#include <unordered_map>
+#include <unordered_set>
+#include <algorithm>
+#include <string>
+#include <numeric>
+#include <cmath>
+#include <bitset>
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main()
+{
+	return 0;
+}
