8f54d721afb1b843c20cbc8bd782326006b2b40b
Zhao Wei <zhaowei.pku@gmail.com>
2015-08-23 15:35:02 +0800

    Googol String large - ac

--- 2016 Round A A-Googol String.cpp
+++ 2016 Round A A-Googol String.cpp
@@ -14,6 +14,69 @@
 
 using namespace std;
 
+vector<long long> candidates;
+
+int main()
+{
+	ifstream fin;
+	fin.open("C:\\Users\\Zhao Wei\\Downloads\\A-large.in");
+	ofstream fout;
+	fout.open("C:\\Users\\Zhao Wei\\Downloads\\A-large.out");
+	
+	for (int i = 0; i <= 60; i++)
+	{
+		long long k = 1;
+		candidates.push_back(k << i);
+	}
+
+	int test_num = 0;
+	fin >> test_num;
+	for (int p = 0; p != test_num; p++)
+	{
+		
+		long long a = 0;
+		fin >> a;
+		fout << "Case #" << p+1 << ": ";
+		int reverse_cnt = 0;
+
+		bool flg = false;
+		for (int i = 60; i >= 0; i-- )
+		{
+			if (a == candidates[i])
+			{
+				fout << "0" << endl;
+				flg = true;
+				break;
+			}
+		}
+		if (flg)	continue;
+
+		for (int i = 60; i >= 0; i-- )
+		{
+			if (a > candidates[i])
+			{
+				a -= candidates[i];
+				a = candidates[i] - a;
+				reverse_cnt++;
+			}
+			else if (a == candidates[i])
+			{
+				if (reverse_cnt % 2)	fout << "1" << endl;
+				else	fout << "0" << endl;
+				flg = true;
+				break;
+			}
+		}
+		if (flg)	continue;
+
+		if (reverse_cnt % 2)	fout << "1" << endl;		
+		else					fout << "0" << endl;
+	}
+	fin.close();
+	fout.close();
+	return 0;
+}
+/*
 void switchString(string& s)
 {
 	for (int i = 0; i != s.size(); i++)
@@ -54,4 +117,5 @@ int main()
 	fout.close();
 	return 0;
 }
+*/
 
